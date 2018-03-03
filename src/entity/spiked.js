d81e2a6c7e9b53480a5ce001efc000f8db66df6f
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2018-03-03 15:35:39 -0600

    Update spiked.js

--- src/entity/spiked.js
+++ src/entity/spiked.js
@@ -31,7 +31,47 @@ PlayerCell.prototype.calcMergeTime = function(base) {
 // Override
 
 PlayerCell.prototype.onConsume = function(consumer,gameServer) {
+   
+  var client = consumer.owner;
+    var maxSplits = Math.floor(consumer.mass/16) - 1; // Maximum amount of splits
+    var numSplits = 600 - client.cells.length; // Get number of splits
+    numSplits = Math.min(numSplits,maxSplits);
+    var splitMass = Math.min(consumer.mass/(numSplits + 1), 32); // Maximum size of new splits
+    
+    // Cell consumes mass before splitting
     consumer.addMass(this.mass);
+    
+    // Cell cannot split any further
+    if (numSplits <= 0) {
+        return;
+    }
+    
+    // Big cells will split into cells larger than 32 mass (1/4 of their mass)
+    var bigSplits = 0;
+    var endMass = consumer.mass - (numSplits * splitMass);
+    if ((endMass > 300) && (numSplits > 0)) {
+        bigSplits++;
+        numSplits--;
+    } 
+    if ((endMass > 1200) && (numSplits > 0)) {
+        bigSplits++;
+        numSplits--;
+    }
+    
+    // Splitting
+    var angle = 0; // Starting angle
+    for (var k = 0; k < numSplits; k++) {
+        angle += 6/numSplits; // Get directions of splitting cells
+        gameServer.newCellVirused(client, consumer, angle, splitMass,150);
+        consumer.mass -= splitMass;
+    }
+    
+    for (var k = 0; k < bigSplits; k++) {
+        angle = Math.random() * 6.28; // Random directions
+        splitMass = consumer.mass / 4;
+        gameServer.newCellVirused(client, consumer, angle, splitMass,18);
+        consumer.mass -= splitMass;
+    }
 }
 
 PlayerCell.prototype.onAdd = function(gameServer) {
