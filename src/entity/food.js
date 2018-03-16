a80ab6c229ae64e8ba49a3839bb50926cd38ee0e
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2018-03-16 13:34:48 -0500

    Update food.js

--- src/entity/food.js
+++ src/entity/food.js
@@ -21,7 +21,7 @@ Virus.prototype.feed = function(feeder,gameServer) {
     // Check if the virus is going to explode
     if (this.mass >= gameServer.config.virusBurstMass) {
         this.mass = gameServer.config.virusStartMass; // Reset mass
-        gameServer.shootVirus(this);
+        gameServer.shootEject(this);
     }
 	
 }
